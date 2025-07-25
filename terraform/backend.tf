9181b237fdb3cf7f9f9730a807ef222477b18c9f
Debesh Behera <anshubehera112@gmail.com>
2025-07-25 19:51:03 +0530

    update terraform and backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tera-statebucket"
+    bucket = "debesh-terabucket"
     key    = "statefiles/terraform.tfstate"
     region = "ap-south-1"
   }
--- terraform/modules/codebuild/main.tf
+++ terraform/modules/codebuild/main.tf
@@ -14,6 +14,6 @@ resource "aws_codebuild_project" "build" {
 
   source {
     type      = "CODEPIPELINE"
-    buildspec = "buildspec.yml"
+    buildspec = "buildyamlfile/buildspec.yml"
   }
 }
